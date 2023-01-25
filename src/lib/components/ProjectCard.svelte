eb246961eec480b45275a8f8e8c1d3c2746478ce
Forrest S <thewanderinwolves@gmail.com>
2023-01-25 11:06:03 -0800

    new bio added.

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,21 @@
+<script>
+  import RevealOnScroll from "../utils/RevealOnScroll.svelte";
+  import ProjectContent from "./ProjectContent.svelte";
+  export let projects;
+</script>
+
+<section class="gutter py-16 lg:py-32 xl:py-64">
+  <h3
+    class="text-center text-primaryColor text-2xl font-black font-fraunces uppercase tracking-[0.25rem] md:py-10 md:text-3xl underline"
+  >
+    Personal Ventures
+  </h3>
+  <!-- <hr class='border-primaryColor border-2'/> -->
+  <section class="md:grid lg:grid-cols-2 gap-10 max-w-[1400px] mx-auto">
+    {#each projects as project (project.id)}
+      <RevealOnScroll let:isVisible>
+        <ProjectContent {project} />
+      </RevealOnScroll>
+    {/each}
+  </section>
+</section>
