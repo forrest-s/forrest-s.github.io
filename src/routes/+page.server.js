81661ce186a76acf60563cd9603602dfb80f549b
Forrest S <thewanderinwolves@gmail.com>
2023-03-13 14:00:42 -0700

    added new projects.

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -52,7 +52,7 @@ export function load() {
       content:
         "This full-stack application connects to The-One-API to retrieve character information from the Lord of the Rings franchise. Each card displays detailed character information and, if available, famous movie quotes. The front-end is built using React, while the back-end is powered by Node.js and Express.",
       title: "Characters of the Lord of the Rings",
-      technologies: ['React'],
+      technologies: ['React', 'Surge'],
       id: 1,
     },
     {
@@ -61,7 +61,7 @@ export function load() {
       content:
         "This is a full-stack application that utilizes a database for storing bounties. Users can add bounties to the database and track their progress. The front-end is built using React, while the back-end is powered by Node.js and Express. The database is managed using MongoDB and Mongoose, which facilitates schema creation.",
       title: "Bounty Hunter",
-      technologies: ['React', 'Express.js', 'Node.js'],
+      technologies: ['React', 'Express.js', 'Node.js', 'Heroku'],
       id: 2,
     },
     {
@@ -70,7 +70,7 @@ export function load() {
       content:
         "This web application serves as a book database, allowing users to add books along with their respective authors and genres. Users can either add a new book or explore existing books, genres, and authors. The front-end of the application is built using the React library, with react-router facilitating the routing of different views. The back-end is powered by the Express/Node.js framework, with MongoDB as the database for book storage. The Mongoose library is used for schema creation and object-document mapping. The application also features authentication via token-based encryption. The application is live on the Heroku platform.",
       title: "Books We've Read Database",
-      technologies: ['React', 'Express.js', 'Node.js', 'MongoDB'],
+      technologies: ['React', 'Express.js', 'Node.js', 'MongoDB', 'Heroku'],
       id: 3,
     },
     {
@@ -79,9 +79,27 @@ export function load() {
       content:
         "The Exercise Tracker application incorporates authentication and login features to enable users to securely track their workouts and participate in the forum. The front-end of the application is built with React, utilizing react-router for effective routing, while the back-end is powered by Node.js and Express. MongoDB serves as the database, with Mongoose supporting schema creation and object-document mapping. The app ensures security through token-based authentication and encryption. Additionally, the application is hosted live on Heroku for easy access.",
       title: "Exercise Tracker/Forum",
-      technologies: ['React', 'Express.js', 'Node.js', 'MongoDB'],
+      technologies: ['React', 'Express.js', 'Node.js', 'MongoDB', 'Heroku'],
       id: 4,
-    }
+    },
+    {
+      image: `${base}/images/pokemon.png`,
+      url: 'pokemon-fresh.deno.dev',
+      content:
+        'This pokemon viewing website is created using newer web technologies, deno and fresh. The pokemon site has a pokedex for viewing your favorite pokemon and their stats/moves. This site also gathers the pokemon informatin from a third party api call. This site is deployed using deno deploy.',
+      title: "Pokedex",
+      technologies: ['Deno', 'Fresh', 'Deno Deploy'],
+      id: 5,
+    },
+    {
+      image: `${base}/images/lotrSvelte.png`,
+      url: 'pokemon-fresh.deno.dev',
+      content:
+        'This is another lord of the rings character viewing website. Instead, I tried diffferent technologies and improved on the design. This lotr character site uses svelte as a compiler and sveltekit as a metaframework deployed on  vercel.',
+      title: "Lotr Characters",
+      technologies: ['Svelte', 'Sveltekit', 'Vercel'],
+      id: 6,
+    }    
   ];
 
   const gallery = [
